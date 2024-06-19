3d73ba43d03182927b0482cb24b4c73cc9db6f59
Preuss Jan-Henrik <jan-henrik.preuss@stud.hslu.ch>
2024-06-19 09:19:39 +0200

    Use svg as formula format

--- make.sh
+++ make.sh
@@ -1,5 +1,4 @@
 # Generate a pdf using pandoc
 # pandoc CHEATSHEET.md -f markdown -t pdf -o CHEATSHEET.pdf  -V geometry:margin=0.5in
-
 pandoc -t asciidoc -f markdown CHEATSHEET.md > out/CHEATSHEET.adoc
-docker run --rm --privileged -u $(id -u):$(id -g) -v `pwd`:/documents asciidoctor/docker-asciidoctor asciidoctor-pdf -a pdf-page-layout=landscape -a stem -r asciidoctor-mathematical out/CHEATSHEET.adoc
+docker run --rm --privileged -u $(id -u):$(id -g) -v `pwd`:/documents asciidoctor/docker-asciidoctor asciidoctor-pdf -a pdf-page-layout=landscape -a stem -r asciidoctor-mathematical -a mathematical-format=svg out/CHEATSHEET.adoc 
